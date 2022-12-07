1734574688ed5a985ab2b3be93a01a5309bc1ef1
SusyVP <109108513+SusyVP@users.noreply.github.com>
2022-12-06 19:28:20 -0600

    update docker

--- docker.sh
+++ docker.sh
@@ -55,37 +55,3 @@ echo "deb http://packages.azlux.fr/debian/ buster main" | sudo tee /etc/apt/sour
 wget -qO - https://azlux.fr/repo.gpg.key | sudo apt-key add -
 sudo apt update
 sudo apt install docker-ctop
-
-echo '=============================================='
-echo '=== PASO 8: CREAR DIRECTORIOS DE VOLUMENES ==='
-echo '=============================================='
-sudo mkdir -p /volumes/www/
-sudo mkdir -p /volumes/nginx/certs
-sudo mkdir -p /volumes/nginx/vhostd
-sudo mkdir -p /volumes/nginx/config
-sudo mkdir -p /volumes/nginx/html
-cd /volumes/www
-
-echo '=================================================='
-echo '=== PASO 9: CLONAR RESPOSITORIOS DE SITIOS WEB ==='
-echo '=================================================='
-sudo git clone https://github.com/StartBootstrap/startbootstrap-sb-admin-2.git
-sudo git clone https://github.com/Flicomastic/powerjgym.git
-sudo git clone https://github.com/microsoft/project-html-website.git
-
-sudo mv powerjgym portal
-sudo mv startbootstrap-sb-admin-2 erp
-sudo mv project-html-website factura
-
-echo '================================================'
-echo '=== PASO 10: CLONAR REPOSITORIO CONTENEDORES ==='
-echo '================================================'
-cd /volumes
-sudo git clone https://github.com/SusyVP/MRSI.git
-sudo mv MRSI deploy
-sudo cp deploy/http/erp.html www/erp/index.html
-sudo cp deploy/http/factura.html www/factura/index.html
-echo '================================'
-echo '=== PASO 11: REALIZAR DEPLOY ==='
-echo '================================'
-docker-compose -f deploy/docker-compose.yml up -d
